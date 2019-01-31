b7abc09ad5471286354ad9f38bcff0eaa9cdb897
cezar <cezarh1@hotmail.com>
2019-01-30 23:01:06 -0200

    Adicionando tudo

---
+++ WebApplication1/WebApplication1/Pages/Edit.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        public EditModel(AppDbContext db) { _db = db; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
